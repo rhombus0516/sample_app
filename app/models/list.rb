cd85d319a16298888738077ab353314a77a0e5ed
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-08-10 04:43:37 +0000

    [Add]バリデーション設定

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,7 @@
 class List < ApplicationRecord
     has_one_attached :image
+    
+    validates :title, presence: ture
+    validates :body, presence: ture
+    validates :image, prasence: ture
 end
