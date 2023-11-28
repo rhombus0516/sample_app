24e4432367e7d077a43bf68a7a8931cef8fd4955
rhombus0516 <karaya0913@gmail.com>
2023-11-28 01:28:55 +0000

    [Update]route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'top' => 'homes#top'
-  resources :lists
+  get '/top' => 'homes#top'
+  resources :lists, only: [:index, :edit, :new, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
